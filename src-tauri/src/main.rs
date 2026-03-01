33e57068398355508a36638e55ab21ff08cd79fe
yinshuxun <sxyin@alauda.io>
2026-03-02 01:49:55 +0800

    chore: bump version to 0.5.5

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,7 +6,7 @@ use xskill_lib::{ide_sync, scaffold, skill_manager};
 
 #[derive(Parser)]
 #[command(name = "xskill")]
-#[command(version = "0.5.1")]
+#[command(version = "0.5.5")]
 #[command(about = "XSkill CLI", long_about = None)]
 struct Cli {
     #[command(subcommand)]
